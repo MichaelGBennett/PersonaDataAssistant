f40d5e45071e58bd229121f00ce952f884dbfdfd
MGlennUA <62678834+MGlennUA@users.noreply.github.com>
2023-12-05 14:40:57 -0700

    spring boot init

---
+++ src/main/java/com/bennett/PersonalDataAssistant/PersonalDataAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.bennett.PersonalDataAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDataAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDataAssistantApplication.class, args);
+	}
+
+}
